de10a123ab53378ae223a683bcb1675533cb0c2e
yuvalavik <116814174+yuvalavik@users.noreply.github.com>
2023-06-07 18:01:18 +0300

    register

--- app/src/main/java/com/example/friends/MainActivity.java
+++ app/src/main/java/com/example/friends/MainActivity.java
@@ -1,14 +1,118 @@
 package com.example.friends;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST_CODE = 1;
+
+    private ImageView selectedImage;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private EditText confirmPasswordEditText;
+    private EditText displayNameEditText;
+    private Bitmap selectedBitmap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectedImage = findViewById(R.id.selected_image);
+        usernameEditText = findViewById(R.id.edit_username_reg);
+        passwordEditText = findViewById(R.id.edit_password_reg);
+        confirmPasswordEditText = findViewById(R.id.confirm_password);
+        displayNameEditText = findViewById(R.id.display_name_input);
+
+        Button registerButton = findViewById(R.id.reg_btn);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
     }
+
+    private void registerUser() {
+        // Get user details
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+        String displayName = displayNameEditText.getText().toString();
+
+        // Validate input
+        if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || displayName.isEmpty()) {
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Validate password and confirm password match
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    public void onAddPictureButtonClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+
+            // Create a Bitmap from the selected image URI
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (bitmap != null) {
+                // Resize the bitmap to maximum dimensions of 200x200 while preserving aspect ratio
+                int maxWidth = 300;
+                int maxHeight = 300;
+
+                int imageWidth = bitmap.getWidth();
+                int imageHeight = bitmap.getHeight();
+
+                if (imageWidth > 0 && imageHeight > 0) {
+                    float scale = Math.min(((float) maxWidth / imageWidth), ((float) maxHeight / imageHeight));
+                    Matrix matrix = new Matrix();
+                    matrix.postScale(scale, scale);
+                    Bitmap resizedBitmap = Bitmap.createBitmap(bitmap, 0, 0, imageWidth, imageHeight, matrix, true);
+
+                    selectedImage.setImageBitmap(resizedBitmap);
+                    selectedImage.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+    }
+
+
 }
